e82875c87c305ab8ef1250499f2f925da673e9c5
Sem8 <mssemmi8@gmail.com>
2019-02-11 18:02:19 -0500

    work in progress MVP

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,26 @@
 import React from 'react';
 import './Header.css';
+
+import HeaderTitle from './HeaderTitle';
+
+
+
+
+function ImageThumbnail() {
+    return (        
+            <img src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png"></img>   
+    );    
+}
+
+function HeaderContainer() {
+    return(
+        <div className='headerContainer'>
+        <ImageThumbnail />
+        <HeaderTitle />
+        
+    </div>
+    )
+    
+}
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+
+import HeaderContent from './HeaderContent';
+
+function HeaderTitle() {
+    return (
+        <div className='headerTitle'>
+            <h1 >Lambda School</h1>
+            <h4>@LambdaSchool</h4>
+            <h4>26 jan</h4> 
+            <HeaderContent />          
+        </div>        
+    );
+}
+
+export default HeaderTitle;
